d1824e7027f990ad6955a4c551e49b274c01a2aa
yngridcfsouza <isnotlive.yngrid@gmail.com>
2025-03-14 19:09:59 -0300

    feat(users): adicionando a fucionalidade de 'update user' e 'delete user'
    Implementação das funcionalidades para alteração

---
+++ NODE-FUNDAMENTOS/MYCONTACTS/VERSAO-PURO-JS/src/controllers/UserController.js
@@ -0,0 +1,87 @@
+// O Controller serve para armazenar a regra de negócio da aplicação
+// Utilizando as regras do CRUD (Create, Read, Update e Delete 
+
+const { error } = require('console');
+let users = require('../mocks/users');
+
+module.exports = {
+    listUsers(request, response) {
+        const { order }  = request.query;
+
+        const sortedUsers = users.sort((a, b) => {
+            if (order === 'desc') {
+                return a.id < b.id ? 1 : -1;
+            } 
+
+            return a.id > b.id ? 1 : -1;
+
+        });
+
+        response.send(200, sortedUsers);
+    },
+
+    getUserById(request, response) {
+        const { id } = request.params;
+
+        const user = users.find((user) => user.id === id);
+
+        if (!user) {
+            return response.send(400, { error: 'User not found' });
+        }
+        
+        response.send(200, user);
+    },
+
+    createUser(request, response) {
+        const { body } = request; // ou request.body;
+
+        const lastUserId = users[users.length - 1].id;
+
+        const newUser = {
+            id: lastUserId + 1,
+            name: body.name,
+        };
+
+        users.push(newUser);
+
+        response.send(200, newUser);
+
+    },
+
+    updateUser(request, response) {
+        let { id } = request.params;
+        const { name } = request.body;
+
+        id = Number(id);
+
+        const userExists = users.find((user) => user.id === id);
+
+        if (!userExists) {
+            return response.send(400, { error: 'User not found'});
+        }
+
+        users = users.map((user) => {
+            if (user.id === id) {
+                return {
+                    ...user,
+                    name,
+                };
+            }
+
+            return user;
+
+        });
+
+        response.send(200, { id, name })
+
+    },
+
+    deleteUser(request, response) {
+        let { id } = request.params;
+        id = Number(id);
+
+        users = users.filter((user) => user.id !== id);
+
+        response.send(200, { deleted: true });
+    }
+};
