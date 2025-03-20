9d43cdd01ef2e53c9900ef6b022d61a8a355d8f9
yngridcfsouza <isnotlive.yngrid@gmail.com>
2025-03-20 11:46:19 -0300

    feat: adicionar integração com banco de dados e validação de categorias
    Implementada integração com banco de dados PostgreSQL para gerenciamento de categorias;
    E foram adicionados métodos para verificar nomes de categorias duplicados ao criar ou atualizar categorias.

---
+++ NODE-FUNDAMENTOS/MYCONTACTS/VERSAO-COM-DOCKER/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+require('express-async-errors');
+
+const router = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(router);
+app.use((error, request, response, next) => {
+  console.log('##### Error Handler');
+  console.log(error);
+  response.sendStatus(500);
+});
+
+app.listen(3000, () => console.log('Server started at http://localhost:3000'));
